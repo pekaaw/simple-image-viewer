7df119135af65547d080ecc1ed99a7b422d8b355
PK <pekaaw@hotmail.com>
2023-05-25 01:39:21 +0200

    Update comments in powershell script

--- run.ps1
+++ run.ps1
@@ -1,15 +1,15 @@
 # Get reference to all image files
-$images = Get-ChildItem -Recurse -Include *.jpg,*.jpeg,*.png . | Resolve-Path -Relative
+$images = Get-ChildItem -Recurse -Include *.jpg,*.jpeg,*.png
 
 # Create empty array
 $imageArray = @()
 
-# for each image, add filename and extension to array (file.extension)
+# for each image, add relative path to array (./*/file.extension)
 foreach ($image in $images) {
     $imageArray += ($image | Resolve-Path -Relative).Replace('\', '/')
 }
 
-# Convert array to json and export to images.json
+# Convert array to json and export to images.js as a javascript variable
 $fileContent = "var images = " + ($imageArray | ConvertTo-Json) + ";"
 $fileContent > images.js
 
